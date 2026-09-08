2ee6195ae1ff1286c6cfbec9d0e735419b15a419
Ambrella <154675435+AmbreIIa@users.noreply.github.com>
2026-09-08 20:49:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Моя перша сторінка</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+            
+            echo "Привіт User"; 
+        ?>
+    </h1>
+
+</body>
+</html>
